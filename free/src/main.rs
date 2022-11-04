f17bb06248c5164a6e78409e2897bb7dd0d29c04
weru <fromweru@gmail.com>
2022-11-04 18:31:47 +0300

    try advanced traits
    Signed-off-by: weru <fromweru@gmail.com>

--- free/src/main.rs
+++ free/src/main.rs
@@ -1,4 +1,5 @@
 use std::slice;
+use std::ops::Add;
 
 static mut COUNTER: u32 = 0;
 
@@ -17,6 +18,13 @@ fn main() {
     unsafe {
         println!("COUNTER: {}", COUNTER);
     }
+
+    assert_eq!(
+        Point { x: 2, y: 3 } + Point { x: 5, y: 0 },
+        Point { x: 7, y: 3 }
+    );
+
+    make_him_fly();
 }
 
 fn add_to_count(inc: u32) {
@@ -54,3 +62,65 @@ fn unsafe_split() {
     // println!("{:#?}", b);
 }
 
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Add for Point {
+    type Output = Point;
+
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+struct Millimeters(u32);
+struct Meters(u32);
+
+impl Add<Meters> for Millimeters {
+    type Output = Millimeters;
+
+    fn add(self, other: Meters) -> Millimeters {
+        Millimeters(self.0 + (other.0 * 1000))
+    }
+}
+
+trait Pilot {
+    fn fly(&self);
+}
+
+trait Wizard {
+    fn fly(&self);
+}
+
+struct Human;
+
+impl Pilot for Human {
+    fn fly(&self) {
+        println!("This is your captain speaking.");
+    }
+}
+
+impl Wizard for Human {
+    fn fly(&self) {
+        println!("Up!");
+    }
+}
+
+impl Human {
+    fn fly(&self) {
+        println!("umh, waving arms furiously");
+    }
+}
+
+fn make_him_fly() {
+    let person = Human;
+    Pilot::fly(&person);
+    Wizard::fly(&person);
+    person.fly();
+}
