c67f6f92b7ee171ccd5d3beb2afc25e07ce74269
weru <fromweru@gmail.com>
2022-11-14 18:10:14 +0300

    cleanup server
    Signed-off-by: weru <fromweru@gmail.com>

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,8 +2,6 @@ use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
-    thread,
-    time::Duration,
 };
 
 use server::ThreadPool;
@@ -12,13 +10,15 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(6) {
         let stream = stream.unwrap();
 
         pool.execute(|| {
             handle_connection(stream);
         });
     }
+
+    println!("Shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
